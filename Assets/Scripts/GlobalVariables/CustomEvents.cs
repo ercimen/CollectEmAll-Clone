5e31fd1b3c6dbdc428b999ffcf478f18cfb3c8d7
ercimen <33921954+ercimen@users.noreply.github.com>
2023-03-19 20:37:56 +0300

    Update CustomEvents.cs

--- Assets/Scripts/GlobalVariables/CustomEvents.cs
+++ Assets/Scripts/GlobalVariables/CustomEvents.cs
@@ -9,26 +9,8 @@ public static class CustomEvents
     /// <example> <code> public const string SomeEvent = nameof(SomeEvent); </code> </example>
     /// </summary>
 
-    #region ObjectPool
-    public const string onGetCube = nameof(onGetCube);
-    public const string onReleaseCube = nameof(onReleaseCube);
+    #region TileEvents
+    public const string onResetSelectableStatus = nameof(onResetSelectableStatus);
     #endregion
-
-    #region Cube
-    public const string onGetCubeRigidbody = nameof(onGetCubeRigidbody);
-    #endregion
-
-    #region GameManager
-    public const string onNextLevel = nameof(onNextLevel);
-    public const string onLevelStart = nameof(onLevelStart);
-    public const string onRestartLevel = nameof(onRestartLevel);
-    public const string onLevelFailed = nameof(onLevelFailed);
-    #endregion
-
-    #region LevelManager
-    public const string onReadyNewLevel = nameof(onReadyNewLevel);
-    #endregion
-
-
 
 }
